5514c3b3d08c99e6d2b87092b51fcc5522cdfe24
matibjolivera <matibjolivera@gmail.com>
2021-06-02 20:11:54 -0300

    Update form.js

--- src/form.js
+++ src/form.js
@@ -1,12 +1,3 @@
 import Vue from "vue";
 import VueForm from "vue-form";
-/*
-const options = {
-    validators: {
-        "no-espacios": function (v) {
-            return !v.includes(' ')
-        }
-    }
-}*/
-
 Vue.use(VueForm)
